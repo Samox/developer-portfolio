b2c3d79577c1d1472c80a589802efed2aea231de
Sammy Teillet <sammy.teillet@gmail.com>
2024-03-26 17:40:37 +0100

    chore: migrate to vite

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,9 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
-import "./css/card.scss";
-import "./css/globals.scss";
-// import Home from "./page";
-
-export const metadata = {
-  title: "Portfolio of Abu Said",
-  description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
-};
+import "./css/card.css";
+import "./css/globals.css";
+import Home from "./page";
 
 function App() {
   return (
@@ -18,7 +12,7 @@ function App() {
       <ToastContainer />
       <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
         <Navbar />
-        {/* <Home /> */}
+        <Home />
       </main>
       <Footer />
     </>
