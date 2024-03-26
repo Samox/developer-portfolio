e79c56eeb04648c0180065a77ea85da51f331b10
Sammy Teillet <sammy.teillet@gmail.com>
2024-03-26 12:05:07 +0100

    chore : add utils

---
+++ utils/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+  email: "sammy.teillet@gmail.com",
+  phone: "+33683919393",
+  address: "Nantes, 44300, France",
+  github: "https://github.com/samox",
+  linkedIn: "https://www.linkedin.com/in/sammy-teillet-30b97963/",
+};
---
+++ utils/data/personal-data.js
@@ -0,0 +1,13 @@
+export const personalData = {
+  name: "SAMMY TEILLET",
+  profile: "/profile.jpeg",
+  designation: "Software Developer",
+  description:
+    "My name is Sammy Teillet. I'm a software developer and inventor, based in Nantes, France. My favorite color is green. I love spicy food. I code everyday. I'm fullstack, with a preference for backend and infrastructure topics. I'm driven by solving people's problem.",
+  email: "sammy.teillet@gmail.com",
+  phone: "+33683919393",
+  address: "Nantes, 44300, France",
+  github: "https://github.com/samox",
+  linkedIn: "https://www.linkedin.com/in/sammy-teillet-30b97963/",
+  resume: "https://registry.jsonresume.org/samox?theme=elegant",
+};
