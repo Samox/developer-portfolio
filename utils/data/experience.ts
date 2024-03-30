559edd223c3efb221eaff92e5eef4b6630b398f1
Sammy Teillet <sammy.teillet@gmail.com>
2024-03-30 10:34:28 +0100

    feature: update projects

--- utils/data/experience.ts
+++ utils/data/experience.ts
@@ -1,20 +1,26 @@
 export const experiences = [
   {
+    id: 0,
+    title: "CTO Co-founder",
+    company: "DataDrift",
+    duration: "(Jun 2023 - Present)",
+  },
+  {
     id: 1,
+    title: "Freelancer",
+    company: "BPI France, Twenty, Kolet, etc.",
+    duration: "(Jan 2023 - Present)",
+  },
+  {
+    id: 2,
     title: "Head of Engineering",
     company: "Libeo",
     duration: "(Jul 2020 - Jan 2023)",
   },
   {
-    id: 2,
+    id: 3,
     title: "Software Engineer",
     company: "Theodo",
     duration: "(May 2016 - May 2019)",
-  },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)",
   },
 ];
